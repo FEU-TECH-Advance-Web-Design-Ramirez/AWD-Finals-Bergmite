230ab9d1586eee8e383906bffa4b1be0a44704d4
kbanganay <kobebanganay@gmail.com>
2025-04-04 15:05:35 +0800

    feat(sign-up-page): added api and other features

--- pages/sign-up-page/assets/js/scripts.js
+++ pages/sign-up-page/assets/js/scripts.js
@@ -1,4 +1,4 @@
-document.getElementById("signupForm").addEventListener("submit", function (event) {
+document.getElementById("signupForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
     const API_URL = "https://demo-api-skills.vercel.app/api/MusicLover/users";
@@ -9,18 +9,39 @@ document.getElementById("signupForm").addEventListener("submit", function (event
     const confirmPassword = document.getElementById("confirmPassword").value;
 
     if (password !== confirmPassword) {
-        document.getElementById("signupError").style.display = "block";
-        return;
+         document.getElementById("signupError").style.display = "block";
+         return;
     }
 
     axios.post(API_URL, { name, email, password })
         .then(response => {
-            // If all fields are valid
-            alert("Sign up successful! Welcome, " + name);
-            document.getElementById("signupForm").reset(); // Clear the form
+                   // If all fields are valid
+              alert("Sign up successful! Welcome, " + name);    
+              document.getElementById("signupForm").reset(); // Clear the form
         })
         .catch(error => {
             alert(error.message);
             console.error("Error:", error);
         });
 });
+
+
+
+// function fetchUsers() {
+//      const API_URL = "https://demo-api-skills.vercel.app/api/MusicLover/users";
+
+//      axios.get(API_URL)
+//      .then(response => {
+//          const users = response.data;
+//          let outputHTML = "<ul>";
+//          users.forEach(user => {
+//              outputHTML += <li><strong>${user.id}</strong> - ${user.email}</li> ${user.name};
+//          });
+//          outputHTML += "</ul>";
+//          document.getElementById("output").innerHTML = outputHTML;
+//          })
+//          .catch(error => {
+//              document.getElementById("output").innerHTML = "Error fetching users";
+//              console.error("Error:", error);
+//          });
+//  }
